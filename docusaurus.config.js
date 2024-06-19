830c47baa384f9fd0f8533b9fa704928bd5732e7
geroytan <gry.tan51@gmail.com>
2024-06-19 14:23:28 +0800

    Sixth Commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
         title: "My Site",
         logo: {
           alt: "My Site Logo",
-          src: "img/logo.svg",
+          src: "img/logo1.png",
           href: "/docs/intro",
         },
         items: [
